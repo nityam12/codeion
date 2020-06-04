90d6e8317c3ca47d711728286406da4e4369b305
nityam12 <nityam1050@gmail.com>
2020-06-04 13:00:56 -0700

    fix

--- workers/comment_email_worker.js
+++ workers/comment_email_worker.js
@@ -1,14 +1,14 @@
 // const queue = require('../config/kue');
 
-const commentMailer = require('../mailers/comments_mailer');
+// const commentMailer = require('../mailers/comments_mailer');
 
-//worker for this queue
+// //worker for this queue
 
-// emails her is name of queue and proces  fn is exec every time a job is added
-queue.process('emails', function (job, done) {
-  // console.log("email worker is processing a job",job.data);
+// // emails her is name of queue and proces  fn is exec every time a job is added
+// queue.process('emails', function (job, done) {
+//   // console.log("email worker is processing a job",job.data);
 
-  commentMailer.newComment(job.data);
+//   commentMailer.newComment(job.data);
 
-  done();
-});
+//   done();
+// });
