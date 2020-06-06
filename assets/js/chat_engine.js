442f35eaa59a29d1ae388ee51b7eba6967210994
nityam12 <nityam1050@gmail.com>
2020-06-06 14:21:46 -0700

    fx

--- assets/js/chat_engine.js
+++ assets/js/chat_engine.js
@@ -30,7 +30,7 @@ class ChatEngine {
     this.userName = userName;
     this.id1 = id1;
     this.id2 = id2;
-    this.socket = io.connect();
+    this.socket = io.connect('https://gameomania-roxz.ddns.net', { path: '/app1socket' });
     // this.socket = io.connect('https://gameomania-roxz.ddns.net:5000', {
     //   transports: ['websocket'],
     //   upgrade: false,
